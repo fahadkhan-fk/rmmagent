ef0c00d36d239be495c3aed4b51bb028d4a8f79b
Fahad Khan <fk.fahad96@gmail.com>
2026-06-24 10:14:21 -0700

    feat: add file listing api implementation for windows

---
+++ agent/file_browser_windows.go
@@ -0,0 +1,43 @@
+//go:build windows
+
+package agent
+
+import (
+	"os"
+	"syscall"
+	"time"
+)
+
+func ListDirectoryWindows(rawPath string, page, pageSize int) (map[string]interface{}, error) {
+	return listDirectory(rawPath, page, pageSize)
+}
+
+func fileTimes(info os.FileInfo) (modified, created, accessed string) {
+	modified = formatFileTime(info.ModTime())
+	created = modified
+	accessed = modified
+
+	sys, ok := info.Sys().(*syscall.Win32FileAttributeData)
+	if !ok {
+		return modified, created, accessed
+	}
+
+	created = formatFileTime(time.Unix(0, sys.CreationTime.Nanoseconds()))
+	accessed = formatFileTime(time.Unix(0, sys.LastAccessTime.Nanoseconds()))
+	modified = formatFileTime(time.Unix(0, sys.LastWriteTime.Nanoseconds()))
+	return modified, created, accessed
+}
+
+func fileAttributeFlags(_ string, info os.FileInfo, _ string) (hidden, system, readOnly bool) {
+	sys, ok := info.Sys().(*syscall.Win32FileAttributeData)
+	if !ok {
+		readOnly = info.Mode()&0200 == 0
+		return false, false, readOnly
+	}
+
+	attrs := sys.FileAttributes
+	hidden = attrs&syscall.FILE_ATTRIBUTE_HIDDEN != 0
+	system = attrs&syscall.FILE_ATTRIBUTE_SYSTEM != 0
+	readOnly = attrs&syscall.FILE_ATTRIBUTE_READONLY != 0
+	return hidden, system, readOnly
+}
