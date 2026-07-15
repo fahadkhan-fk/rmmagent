0f6a7c78cb6fbb4ff866418666091ced0e06a974
Fahad Khan <fk.fahad96@gmail.com>
2026-07-15 09:49:56 -0700

    feat: add archive download prepare rpc handler

--- agent/rpc.go
+++ agent/rpc.go
@@ -1171,6 +1171,21 @@ func (a *Agent) RunRPC() {
 				}
 				msg.Respond(resp)
 			}(payload)
+
+		case "files_download_archive_prepare":
+			go func(p *NatsMsg) {
+				var resp []byte
+				ret := codec.NewEncoderBytes(&resp, new(codec.MsgpackHandle))
+
+				result, err := a.PrepareFilesDownloadArchive(p)
+				if err != nil {
+					a.Logger.Errorln("files_download_archive_prepare:", err)
+					_ = ret.Encode(map[string]interface{}{"error": err.Error()})
+				} else {
+					_ = ret.Encode(result)
+				}
+				msg.Respond(resp)
+			}(payload)
 		}
 	})
 	nc.Flush()
